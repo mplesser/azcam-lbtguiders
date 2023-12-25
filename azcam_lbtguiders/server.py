da2b509b7c54a525b8c1bb30d9970638e3213ddb
Michael Lesser <mlesser@arizona.edu>
2023-12-25 16:51:22 -0700

    update

--- azcam_lbtguiders/server.py
+++ azcam_lbtguiders/server.py
@@ -21,7 +21,7 @@ from azcam_server.tools.mag.exposure_mag import ExposureMag
 from azcam_server.tools.mag.tempcon_mag import TempConMag
 from azcam_server.tools.ds9display import Ds9Display
 from azcam_server.tools.sendimage import SendImage
-from azcam_server.webserver.fastapi_server import WebServer
+from azcam_webtools.webserver.fastapi_server import WebServer
 from azcam_webtools.status.status import Status
 from azcam_webtools.exptool.exptool import Exptool
 
